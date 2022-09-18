cba94e3ae820e8b7d4b1001b2cc86fac4e74d84b
binarytester <110983840+binarytester@users.noreply.github.com>
2022-09-18 11:17:08 -0300

    Create q7.ts

---
+++ exercicio_02/q7.ts
@@ -0,0 +1,7 @@
+const nome: string = "Ely";
+const language: string = "TypeScript";
+const payment: number = 120.56;
+
+console.log(
+  `${nome}\nMy payment time is ${payment}\nand\nmy preffered language is ${language}`
+);
